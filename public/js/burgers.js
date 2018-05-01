891d088404d1743eba59fb81bc8021254bfdd1cf
Roberto aguero <rjaguero00@gmail.com>
2018-04-30 19:44:07 -0700

    js

---
+++ public/js/burgers.js
@@ -0,0 +1,47 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+
+    $(".create-burger").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#burger-input").val().trim(),
+            devoured: false
+        };
+        $('#burger-input').val('');
+        console.log(newBurger);
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".devour-btn").on("click", function (event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+
+        var newBurgerStatus = {
+            devoured: true
+        };
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newBurgerStatus
+        }).then(
+            function () {
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
